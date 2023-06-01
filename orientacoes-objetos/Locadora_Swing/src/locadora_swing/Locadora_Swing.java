a187c110c79f190b677d2cdd571136d673126a2a
Cliente <cliente@gmail.com>
2023-05-31 21:37:40 -0300

    locadora-swing

---
+++ orientacoes-objetos/Locadora_Swing/src/locadora_swing/Locadora_Swing.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package locadora_swing;
+import java.util.ArrayList;
+import model.DaoVeiculo;
+import model.Veiculo;
+import view.VeiculoView;
+/**
+ *
+ * @author agp
+ */
+public class Locadora_Swing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DaoVeiculo daoVeiculo = new DaoVeiculo();
+        ArrayList<Veiculo> veiculos = daoVeiculo.buscarTodos();
+        for(Veiculo v : veiculos){
+            System.out.println(v.getModelo()
+                    + " - " + v.getMarca());
+        }
+        
+        new VeiculoView().setVisible(true);
+    }
+    
+    
+}
---
+++ orientacoes-objetos/Locadora_Swing/src/model/DaoVeiculo.java
@@ -0,0 +1,168 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
+
+public class DaoVeiculo {
+    private Connection conn;
+    private Statement st;
+
+
+    private void conectar(){
+        try {
+            this.conn = GerenciadorConexao.pegarConexao();
+            st = conn.createStatement();
+        } catch (ClassNotFoundException e1) {
+            System.out.println(e1.getMessage());
+        }catch (SQLException e2) {
+            System.out.println(e2.getMessage());
+        }
+    }
+
+    private void desconectar(){
+        try {
+            st .close();
+            conn.close();
+        }catch (SQLException e2) {
+            System.out.println(e2.getMessage());
+        }
+    }
+
+    public boolean inserir(Veiculo v){
+        boolean resultado = false;
+        try {
+            this.conectar();
+            String comando = "INSERT INTO tb_veiculos VALUES(NULL,"
+                + "'" + v.getMarca() + "', '" + v.getModelo() + "', '"
+                + v.getChassi() + "', '" + v.getAno() + "');";
+            System.out.println(comando);
+            st.executeUpdate(comando);
+            resultado = true;
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir registro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return resultado;
+    }
+
+    public int alterar(Veiculo v){
+        int qtde = 0;
+        try {
+            this.conectar();
+            String comando = "UPDATE tb_veiculos SET "
+                + "marca = '" + v.getMarca() + "' ,"
+                + "modelo = '" + v.getModelo() + "' ,"
+                + "chassi = '" + v.getChassi() + "' ,"
+                + "ano = " + v.getAno()
+                + " WHERE codigo = " + v.getCodigo() + ";";
+            System.out.println(comando);
+            st.executeUpdate(comando);
+            qtde = st.getUpdateCount();
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir registro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return qtde;
+    }
+
+    public ArrayList<Veiculo> buscarTodos(){
+        ArrayList<Veiculo> resultados = new ArrayList<Veiculo>();
+
+        try {
+            this.conectar();
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos ORDER BY marca;");
+            while(rs.next()){
+                Veiculo v = new Veiculo();
+                v.setCodigo(rs.getInt("codigo"));
+                v.setMarca(rs.getString("marca"));
+                v.setModelo(rs.getString("modelo"));
+                v.setChassi(rs.getString("chassi"));
+                v.setAno(rs.getInt("ano"));
+
+                resultados.add(v);
+            }           
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+
+        return resultados;
+    }
+
+
+
+    public Veiculo buscarUm(int cod){
+        Veiculo v = null;
+        try {
+            this.conectar();
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos WHERE codigo = " + cod + ";");
+            if(rs.next()){
+                v = new Veiculo();
+                v.setCodigo(rs.getInt("codigo"));
+                v.setMarca(rs.getString("marca"));
+                v.setModelo(rs.getString("modelo"));
+                v.setChassi(rs.getString("chassi"));
+                v.setAno(rs.getInt("ano"));
+            }           
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return v;
+    }
+
+    
+
+
+    public int excluir (int cod){
+        int qtde = 0;
+        try {
+            this.conectar();
+            String comando = "DELETE FROM tb_veiculos WHERE codigo = " + cod + ";";
+            st.executeUpdate(comando);
+            qtde = st.getUpdateCount();    
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir registro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return qtde;
+    }
+    
+    public ArrayList<Veiculo> filtrar(String opcao, String filtro)
+    {
+        ArrayList<Veiculo> veiculosFiltrados = new ArrayList<Veiculo>();
+        
+        if(!opcao.equals("marca") && !opcao.equals("modelo")){
+            return veiculosFiltrados;
+        }
+        try {
+            this.conectar();
+            ResultSet rs = st.executeQuery("SELECT * FROM tb_veiculos WHERE " + opcao + " LIKE '%" + filtro + "%'; ");
+            while(rs.next()){
+                Veiculo v = new Veiculo();
+                v.setCodigo(rs.getInt("codigo"));
+                v.setMarca(rs.getString("marca"));
+                v.setModelo(rs.getString("modelo"));
+                v.setChassi(rs.getString("chassi"));
+                v.setAno(rs.getInt("ano"));
+
+                veiculosFiltrados.add(v);
+            }           
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return veiculosFiltrados;
+    }
+
+}
